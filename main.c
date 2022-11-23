3dc5ee855044c31b440f560e0ecfe655b61599a0
moriagro <moriagro@gmail.com>
2022-11-23 14:49:26 +0200

    Calling the correct function when there is a letter B or C

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
-// #include "my_mat.h"
+#include "my_mat.h"
+#include "my_mat.c"
 #include <stdio.h>
 #include <ctype.h>
 #ifndef MATRIX_SIZE
 #define MATRIX_SIZE 10
+#endif
+#ifndef PATH_SIZE
+#define PATH_SIZE 2
 #endif
 
 int main()
@@ -10,6 +14,7 @@ int main()
     char info;
     char current_letter;
     int matrix[MATRIX_SIZE][MATRIX_SIZE];
+    int exist_path[PATH_SIZE];
     int current_i = 0;
     int current_j = 0;
     while (scanf("%c", &info) == 1)
@@ -17,10 +22,12 @@ int main()
         if (isalpha(info))
         {
             current_letter = info;
+            current_i = 0;
+            current_j = 0;
         }
         else if (isdigit(info))
         {
-            // int info_num = (int)info;
+            printf("%c\n", current_letter);
             if (current_letter == 'A')
             {
                 // Get matrics from user
@@ -38,13 +45,29 @@ int main()
             else if (current_letter == 'B')
             {
                 // Get i, j to check if there ia a path between i and j
-                // printf("Get i, j to check if there ia a path between i and j");
+                if (current_i < 2)
+                {
+                    exist_path[current_i] = (int)info;
+                    current_i++;
+                }
+                if (current_i == 2)
+                {
+                    existingPath(exist_path[0], exist_path[1]);
+                }
             }
 
             else if (current_letter == 'C')
             {
                 // Get i, j to recieve the sohortest course between them, else return -1
-                // printf("Get i, j to recieve the sohortest course between them, else return -1");
+                if (current_i < 2)
+                {
+                    exist_path[current_i] = (int)info;
+                    current_i++;
+                }
+                if (current_i == 2)
+                {
+                    shortestCourse(exist_path[0], exist_path[1]);
+                }
             }
             else if (current_letter == 'D')
             {
--- my_mat.c
+++ my_mat.c
@@ -1,7 +1,14 @@
-int existingPath(int i, int j){
+#include <stdio.h>
+#include "my_mat.h"
 
+int existingPath(int i, int j)
+{
+    printf("(i, j):  (%c, %c)\n", i, j);
+    return 0;
 }
 
-int shortestCourse(int i, int j){
-
+int shortestCourse(int i, int j)
+{
+    printf("---(i, j):  (%c, %c)\n", i, j);
+    return 0;
 }
